0d60d2bdc315efdfc4dda8c029d9edc2f71874f0
gabowling37 <gabowling37@gmail.com>
2026-09-06 18:54:09 -0400

    Added RecruitCatBowlingy.

---
+++ RecruitCatBowlingy/RecruitCatBowlingy/Pages/OurTeam.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecruitCatBowlingy.Pages
+{
+    public class OurTeamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
