6572b27b4da66ab5d24f490592ba164bb158cc30
gabowling37 <gabowling37@gmail.com>
2026-09-08 19:15:21 -0400

    Add Week 3 folder.

---
+++ Week3/BankAccountsDirectory/Week3Exercise1Bowlingy/Class.cs
@@ -0,0 +1,24 @@
+﻿namespace Week3Exercise1Bowlingy
+{
+    public class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int AccountHolderId { get; set; }
+
+    }
+
+    public class AccountHolder
+    {
+        public int AccountHolderId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<BankAccount> BankAccounts { get; set; }
+    }
+}
---
+++ Week3/MoviesDirectory/MoviesDirectory/Models/Class.cs
@@ -0,0 +1,20 @@
+﻿namespace MoviesDirectory.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public decimal BoxOfficeReceipts { get; set; }
+        public Director? Director { get; set; }
+        public int? DirectorId { get; set; }
+    }
+
+    public class Director
+    {
+        public int DirectorId { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public List<Movie>? Movies { get; set; }
+    }
+}
